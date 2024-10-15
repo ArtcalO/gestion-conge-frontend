941de89b38e42ca8db2198a2123efb7e027c6635
ArtcalO <niyca14@gmail.com>
2024-10-15 16:41:02 +0200

    add all routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createWebHistory, createRouter } from 'vue-router'
+import EmployesView from '@/views/EmployesView.vue'
+import CongeView from '@/views/CongeView.vue'
+
+const routes = [
+  {path:"/employes", component:EmployesView},
+  {path:"/conges", component:CongeView}
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
--- src/views/CongeView.vue
+++ src/views/CongeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view/>
+  <h1>Conge page</h1>
 </template>
 
 <script>
